70df747bff7ecc3ceed567d436fcfc8fc0e3a3f1
gblanco97 <77421946+gblanco97@users.noreply.github.com>
2021-02-07 13:36:41 -0600

    html updates
    updated html on all pages, created js button function

---
+++ functions.js
@@ -0,0 +1,6 @@
+//manip button//
+function briefTitle(){
+    let selectedElement = document.getElementById("mainTitle");
+    console.log(selectedElement);
+    selectedElement.innerText = "Portfolio";
+}
