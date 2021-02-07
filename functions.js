67c911e4773b7465a8c15f5f0a5cd2a119bc731e
gblanco97 <77421946+gblanco97@users.noreply.github.com>
2021-02-07 14:59:40 -0600

    added js function to index.html

--- functions.js
+++ functions.js
@@ -4,3 +4,26 @@ function briefTitle(){
     console.log(selectedElement);
     selectedElement.innerText = "Portfolio";
 }
+
+//time function//
+function currentTime(i){
+    if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+    }
+    
+    function startTime() {
+      var today = new Date();
+      var h = today.getHours();
+      var m = today.getMinutes();
+      // add a zero in front of numbers<10
+      m = checkTime(m);
+      s = checkTime(s);
+      document.getElementById('time').innerHTML = h + ":" + m;
+      t = setTimeout(function() {
+        startTime()
+      }, 500);
+    }
+    startTime();
+    
