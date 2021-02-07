9f27c3779296980f003865119b1fc5dd974c2127
gblanco97 <77421946+gblanco97@users.noreply.github.com>
2021-02-07 15:56:46 -0600

    times

--- functions.js
+++ functions.js
@@ -6,10 +6,8 @@ function briefTitle(){
 }
 
 //time function//
-
-var date = new Date();   // get a new date
-var n = date.toDateString();   // get date as string
-var time = date.toLocaleTimeString();   // get time as string
-// set the innerHTML of that element to the date a space the time
-document.getElementById('time').innerHTML = n + ' ' + time;
+function getTime(){
+    let selectedElement = document.getElementById("time");
+    getTime.push(date.toLocaleTimeString).innerHTML = time;
+}
     
