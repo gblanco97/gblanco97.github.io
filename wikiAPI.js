967457327bbd5d2dec86db4f534a0d9856e54fa5
gblanco97 <77421946+gblanco97@users.noreply.github.com>
2021-02-27 15:37:45 -0600

    update wikiAPI.js

--- wikiAPI.js
+++ wikiAPI.js
@@ -1,5 +1,23 @@
 function wikiAPI() {
+
     var searchTearm = document.getElementById('searchTerm').value;
-    var connect new XMLHttpRequest();
+    var connect = new XMLHttpRequest();
     var url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=20&gsrsearch=" + searchTerm;
+
+    connect.open('GET', url);
+
+    connect.onload = function () {
+        var wikiThing = JSON.parse(this.response);
+        //console.log(wikiThing);
+        //console.log(wikiThing.query.pages);
+        var pages = wikiThing.query.pages;
+        for (var i in pages) {
+            var newDiv = document.createElement("div");
+            newDiv.setAttribute('class', 'row h4',);
+            document.getElementById("wiki").appendChild(newDiv);
+            newDiv.innerText = pages[i].title;
+        };
+    }
+ 
+    connect.send();
 }
