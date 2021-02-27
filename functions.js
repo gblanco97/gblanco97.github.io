2aeef6a4a0dbb53e8c5357ff6e70171d050b8343
gblanco97 <77421946+gblanco97@users.noreply.github.com>
2021-02-27 15:15:59 -0600

    updated pages

--- functions.js
+++ functions.js
@@ -32,38 +32,3 @@ function lastHappy(){
 }
 
 newArray = ["coffee", "sunshine", "snacks", "dogs"];
-
-//map API//
-function mapLoad(){
-    //Define the lat long coordinate
-    var latLng = [41.789649, -87.599702]; 
-    //set attribution and access key url
-    var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
-    var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
-    streets  = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});  
-    //define map object
-    var map = L.map('map', {
-      center: latLng,
-      zoom: 16,
-      layers: [streets]
-    });
-//add tile layers to base layer object, add to map, add a marker with a pop-up
-    var baseLayers = {
-      "Grayscale": grayscale,
-      "Streets": streets
-    };
-    L.control.layers(baseLayers).addTo(map);
-    L.marker(latLng).addTo(map)
-    .bindPopup("<b>UChicago<br>Campus</b>").openPopup();
-//add a nifty event
-    var popup = L.popup();
-    function onMapClick(e) {
-      popup
-      .setLatLng(e.latlng)
-      .setContent("You clicked the map at " + e.latlng.toString())
-      .openOn(map);
-    }
-    map.on('click', onMapClick);
-  }
