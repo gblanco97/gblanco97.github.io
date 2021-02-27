40a8a7295c99b612c43a6ffaf556861352422344
gblanco97 <77421946+gblanco97@users.noreply.github.com>
2021-02-27 13:28:32 -0600

    updated.js

--- functions.js
+++ functions.js
@@ -11,3 +11,14 @@ function getTime() {
     console.log(selectedElement);
     selectedElement.innerText = new Date()
     }
+
+//array function//
+    function parseArray(array) {
+        var happy = prompt("add something that makes you happy"); //ask for input
+        array.push(happy); //.push method adds a value to an array
+        var x = array.sort(); //.sort method sorts values in an array
+        var y = x.length; //.length method accesses the length of an array
+        console.log(x[y-1]); //log the last item in the array
+        console.log(array); //log the entire array
+      }
+      var newArray = ["coffee", "sunshine", "snacks", "dogs"];
